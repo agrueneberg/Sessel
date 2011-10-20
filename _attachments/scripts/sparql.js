9625d55e8500a4677d5122322161fbfcf9bf0f0e
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-10-20 12:25:43 -0500

    Added support for matching multiple graph patterns.

--- _attachments/scripts/sparql.js
+++ _attachments/scripts/sparql.js
@@ -1,7 +1,7 @@
 /*jslint regexp: true */
 var SPARQL = (function () {
     "use strict";
-    var parse, query;
+    var parse, query, graphPatternResolver;
     parse = function (text) {
         var pattern, matcher;
         // Parser for SPARQL queries
@@ -95,28 +95,32 @@ var SPARQL = (function () {
         }
     };
     query = function (queryObject, callback) {
-        var graphPatterns, graphPattern, bindings, bindingPositions, hasBindings, query, xhr;
+        var graphPatterns, initialBindings;
         graphPatterns = queryObject.graphPatterns;
+        initialBindings = [];
+        graphPatternResolver(graphPatterns, initialBindings, callback);
+    };
+    graphPatternResolver = function (graphPatterns, lastBindings, callback) {
+        var graphPatterns, graphPattern, binding, bindingPositions, hasBindings, query, xhr;
         if (graphPatterns.length > 0) {
-            // do one pattern at a time
+            // Process only one pattern at a time.
             graphPattern = graphPatterns.pop();
-            // extract variables to bind
-            bindings = {};
-            bindingPositions = {};
-            hasBindings = false;
+            binding = []; // The binding serves as a template for creating concrete bindings once the triples are retrieved.
+            bindingPositions = {}; // Save the position of the variable in the triple (subject: 0, predicate: 1, object: 2) for easy retrieval later.
+            hasBindings = false; // There is no point in doing another recursion for an empty graph pattern.
             graphPattern.forEach(function (node, index) {
                 var matcher;
                 matcher = node.match(/\?(\w+)/);
                 if (matcher !== null) {
-                    // store bindings using the variable name without ?
-                    bindings[matcher[1]] = [];
-                    // store position of the variable
-                    bindingPositions[index] = matcher[1];
+                    // Store variable names meant for binding without initial "?".
+                    binding.push(matcher[1]);
+                    bindingPositions[matcher[1]] = index;
                     hasBindings = true;
                 }
             });
+            // There has to be at least one binding, otherwise the operation is pointless.
             if (hasBindings) {
-                // extract nodes to query
+                // Create Sessel query string.
                 query = "_rewrite";
                 graphPattern.forEach(function (node, index) {
                     var matcher;
@@ -138,29 +142,70 @@ var SPARQL = (function () {
                 query += "?format=json";
                 xhr = new XMLHttpRequest();
                 xhr.onreadystatechange = function () {
-                    var triples;
+                    var triples, newBindings;
                     if (xhr.readyState === 4) {
+                        newBindings = [];
                         triples = JSON.parse(xhr.responseText);
+                        // Bind triples to binding.
+                        console.log('binding', binding);
                         triples.forEach(function (triple) {
-                            var bindingPosition;
+                            console.log('triple', triple);
+                            var newBinding, bindingPosition;
+                            newBinding = {};
                             triple.splice(2, 1);
-                            for (bindingPosition in bindingPositions) {
-                                // store part of the triple that corresponds with the one stored in bindingPosition in bindings
-                                bindings[bindingPositions[bindingPosition]].push(triple[bindingPosition]);
+                            // If there are no old bindings, just keep the new ones.
+                            if (lastBindings.length === 0) {
+                                console.log('no old bindings found');
+                                // Populate bindings based on triple.
+                                binding.forEach(function (bindingVariable) {
+                                    newBinding[bindingVariable] = triple[bindingPositions[bindingVariable]];
+                                });
+                            } else {
+                                console.log('old bindings found', lastBindings);
+                                lastBindings.forEach(function (lastBinding) {
+                                    var merge, lastBindingVariable;
+                                    // If a binding is present in both bindings, they have to match.
+                                    merge = true;
+                                    binding.forEach(function (bindingVariable) {
+                                        if (binding.hasOwnProperty(bindingVariable)) {
+                                            if (lastBinding.hasOwnProperty(bindingVariable)) {
+                                                if (lastBinding[bindingVariable] !== triple[bindingPositions[bindingVariable]]) {
+                                                    merge = false;
+                                                } else {
+                                                    newBinding[bindingVariable] = triple[bindingPositions[bindingVariable]];
+                                                }
+                                            }
+                                        }
+                                    });
+                                    // Merge old and new bindings.
+                                    if (merge) {
+                                        console.log('merge...', newBinding, lastBinding);
+                                        for (lastBindingVariable in lastBinding) {
+                                            if (lastBinding.hasOwnProperty(lastBindingVariable)) {
+                                                // Only copy if it's not already in newBinding.
+                                                if (!newBinding.hasOwnProperty(lastBindingVariable)) {
+                                                    newBinding[lastBindingVariable] = lastBinding[lastBindingVariable];
+                                                }
+                                            }
+                                        }
+                                    }
+                                });
                             }
+                            newBindings.push(newBinding);
                         });
-                        callback(bindings);
+                        // Carry over the graphPatterns that are left and newBindings over to the next step.
+                        graphPatternResolver(graphPatterns, newBindings, callback);
                     }
                 };
                 xhr.open("GET", query);
                 xhr.send(null);
             } else {
-                // return {} if there are no bindings
-                callback(bindings);
+                // Return the bindings from the last step if there are no bindings.
+                callback(lastBindings);
             }
         } else {
-            // return {} for empty graph pattern
-            callback({});
+            // Return the bindings from the last step if there are no graph patterns (left).
+            callback(lastBindings);
         }
     };
     return {
--- _attachments/tests/sparql.js
+++ _attachments/tests/sparql.js
@@ -1,22 +1,35 @@
 describe("SPARQL", function() {
-    // TODO: set up demo database
+    // TODO: Set up demo database
     // <http://i/mySwift> <http://p/name> "Swift" .
     // <http://i/mySwift> <http://p/type> <http://c/car> .
-    it("should return '{}' for 'SELECT * WHERE {}'", function() {
-        var parsed;
+    it("should return '[]' for 'SELECT * WHERE {}'", function() {
+        var parsed, callback;
+        callback = jasmine.createSpy();
         parsed = SPARQL.parse("SELECT * WHERE {}");
-        SPARQL.query(parsed, function(bindings) {
-            expect(bindings).toEqual({});
+        SPARQL.query(parsed, callback);
+        waitsFor(function () {
+            return callback.callCount > 0;
+        });
+        runs(function () {
+            // TODO: [] and {} are the same?
+            expect(callback).toHaveBeenCalledWith([]);
         });
     });
-    it("should return '{}' for 'SELECT * WHERE {<http://i/mySwift> <http://p/type> <http://c/car>}'", function() {
-        var parsed;
+    it("should return '[]' for 'SELECT * WHERE {<http://i/mySwift> <http://p/type> <http://c/car>}'", function() {
+        var parsed, callback;
+        callback = jasmine.createSpy();
         parsed = SPARQL.parse("SELECT * WHERE {<http://i/mySwift> <http://p/type> <http://c/car>}");
-        SPARQL.query(parsed, function(bindings) {
-            expect(bindings).toEqual({});
+        SPARQL.query(parsed, callback);
+        waitsFor(function () {
+            return callback.callCount > 0;
+        });
+        runs(function () {
+            // TODO: [] and {} are the same?
+            expect(callback).toHaveBeenCalledWith([]);
         });
     });
-    it("should return '{\"a\" : [\"http://c/car\"]}' for 'SELECT * WHERE {<http://i/mySwift> <http://p/type> ?a}'", function() {
+    // One graph pattern
+    it("should return '[{\"a\" : \"http://c/car\"}]' for 'SELECT * WHERE {<http://i/mySwift> <http://p/type> ?a}'", function() {
         var parsed, callback;
         callback = jasmine.createSpy();
         parsed = SPARQL.parse("SELECT * WHERE {<http://i/mySwift> <http://p/type> ?a}");
@@ -25,8 +38,21 @@ describe("SPARQL", function() {
             return callback.callCount > 0;
         });
         runs(function () {
-            expect(callback).toHaveBeenCalledWith({"a" : ["http://c/car"]});
+            expect(callback).toHaveBeenCalledWith([{"a" : "http://c/car"}]);
         });
     });
-    // fail for one variable used twice in a graph pattern
+    // Two graph patterns
+    it("should return '[{\"s\" : \"http://i/mySwift\", \"o\" : \"Swift\"}]' for 'SELECT * WHERE {?s <http://p/type> <http://c/car> . ?s <http://p/name> ?o .}'", function() {
+        var parsed, callback;
+        callback = jasmine.createSpy();
+        parsed = SPARQL.parse("SELECT * WHERE {?s <http://p/type> <http://c/car> . ?s <http://p/name> ?o .}");
+        SPARQL.query(parsed, callback);
+        waitsFor(function () {
+            return callback.callCount > 0;
+        });
+        runs(function () {
+            expect(callback).toHaveBeenCalledWith([{"s" : "http://i/mySwift", "o" : "Swift"}]);
+        });
+    });
+    // Fail for one variable used twice in a graph pattern.
 });

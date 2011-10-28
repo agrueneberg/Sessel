84d282b9a91603c7f26c54fa0f208cdc144c10b7
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-10-28 12:47:24 -0500

    Implemented triple retrieval from IrisCouch.

--- server.js
+++ server.js
@@ -5,6 +5,51 @@ requirejs.config({
     nodeRequire: require
 });
 
-requirejs(["http", "sparql"], function (http, sparql) {
-    console.log("Alive and kicking!");
+requirejs(["http", "url", "sparql"], function (http, url, sparql) {
+    http.createServer(function (req, res) {
+        var parsedUrl, queryObject;
+        // Parse URL including query parameters
+        parsedUrl = url.parse(req.url, true);
+        // Ignore favicon.ico requests
+        if (parsedUrl.pathname === "/favicon.ico") {
+            res.writeHead(404, "text/plain");
+            res.end();
+        } else {
+            // Ignore requests without a query parameter
+            if (parsedUrl.query.query === undefined) {
+                res.writeHead(400, "text/plain");
+                res.end("query parameter needed.");
+            } else {
+                try {
+                    queryObject = sparql.parse(parsedUrl.query.query);
+                    sparql.query(queryObject, function (query, callback) {
+                        // The graph pattern resolver
+                        http.get({
+                            host: "agrueneberg.iriscouch.com",
+                            port: 80,
+                            path: "/sessel/_design/sessel/" + query
+                        }, function(res) {
+                            var body;
+                            body = [];
+                            res.setEncoding('utf8');
+                            res.on('data', function (chunk) {
+                                body.push(chunk);
+                            });
+                            res.on('end', function () {
+                                callback(JSON.parse(body.join("")));
+                            });
+                        });
+                    }, function (bindings) {
+                        // The callback
+                        res.writeHead(200, {"Content-Type": "application/json"});
+                        res.end(JSON.stringify(bindings));
+                    });
+                } catch (e) {
+                    res.writeHead(400, "text/plain");
+                    res.end(e.message);
+                }
+            }
+        }
+    }).listen(80);
+    console.log("Running...");
 });

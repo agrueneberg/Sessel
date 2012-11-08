443aeb8ba79bd2c626e98801ed72582f3614876b
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-11-08 10:41:06 -0600

    Fill in default values for Base URI and Prefix rather than displaying meaningless placeholders.

--- _attachments/scripts/export.js
+++ _attachments/scripts/export.js
@@ -1,12 +1,12 @@
 $(function () {
-    var loc, exportUri;
-    loc = window.location;
-    exportUri = loc.protocol + "//" + loc.host + "/" + loc.pathname.split("/")[1] + "/_design/sessel/_rewrite/export.";
+    var baseUri;
+    baseUri = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname.split("/")[1];
+    $("#base-uri").val(baseUri + "/_design/sessel/_rewrite/");
     $(".export-btn").click(function (evt) {
         var format, form;
         format = evt.target.dataset.exportFormat;
         form = $("#export-form");
-        form.attr("action", exportUri + format);
+        form.attr("action", baseUri + "/_design/sessel/_rewrite/export." + format);
         form.submit();
         return false;
     });
